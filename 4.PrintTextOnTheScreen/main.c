69293133239ac64ec594f58f3b67a7e8112a579e
Dimitar Mitkov <undeadko@abv.bg>
2021-02-08 19:27:22 +0200

    Lesson 4

---
+++ 4.PrintTextOnTheScreen/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* Print function.
+       "\n" is a new line.
+       "\t" is tab.
+       "\a" makes a sound. Stands for "alert" */
+    printf("Hello world!\n");
+    // Let's try it again
+    printf ("\aThis is pretty cool!\n");
+
+    return 0;
+}
