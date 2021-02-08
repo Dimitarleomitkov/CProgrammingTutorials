fe2efa6d988abc0959be84d26ac6f7b8c283d664
Dimitar Mitkov <undeadko@abv.bg>
2021-02-08 19:27:13 +0200

    Lesson 2

---
+++ 2.HelloWorld/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// This is a comment
+/* This is a multi line comment
+   It looks like this
+   We might need them at some point */
+int main()
+{
+    // This is our first program
+    printf("Hello world!\n");
+    return 0;
+}
