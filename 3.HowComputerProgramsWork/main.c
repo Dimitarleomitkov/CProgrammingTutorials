78843b5a6d77c7d52c2c42d605155399a8e5ef56
Dimitar Mitkov <undeadko@abv.bg>
2021-02-08 19:27:17 +0200

    Lesson 3

---
+++ 3.HowComputerProgramsWork/main.c
@@ -0,0 +1,15 @@
+// These are header files
+#include <stdio.h>
+// They include already written code (functions) from "libraries"
+#include <stdlib.h>
+
+// This is the first function of every program
+int main()
+{
+    /* Inside the "{" and "}" is the scope of the function
+       The code is implemented here. */
+    // Call to the function printf ()
+    printf("Hello world!\n");
+    // This returns a succession code for the program to know whether it failed
+    return 0;
+}
